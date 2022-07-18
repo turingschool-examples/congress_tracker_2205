984ee50681d78e044462982d5c1021c507fa33c4
Juliet Eyraud <juliet@turing.edu>
2022-07-18 10:50:30 -0500

    Add login routes to make test pass

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   resources :users, only: [:new, :create]
+  get '/login', to: 'users#login_form'
+  post '/login', to: 'users#login'
   post '/search', to: 'congress#search'
   post '/search_state', to:'congress#index'
 end
